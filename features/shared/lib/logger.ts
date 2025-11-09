f2f84b42f66d1fb1f4db9223e631031cb7a8fb27
Diego Grassino <diegograssino@gmail.com>
2025-11-08 21:11:13 -0300

    Refactor responsive styles

--- features/shared/lib/logger.ts
+++ features/shared/lib/logger.ts
@@ -1,6 +1,6 @@
 import { LogContext } from "@/types";
 import pino from "pino";
-// TODO Add log support
+// TODO Add proper log support
 
 const config = {
   serverUrl: process.env.REACT_APP_API_PATH || "http://localhost:3000",
