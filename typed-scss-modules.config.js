56efba0233a3c3116c9170cbafaff157b684469a
Diego Grassino <diegograssino@gmail.com>
2025-11-28 23:09:06 -0300

    Clean up unnecesary cursor comments mostly

--- typed-scss-modules.config.js
+++ typed-scss-modules.config.js
@@ -1,19 +1,9 @@
 module.exports = {
-  // Export type: 'default' or 'named'
-  // Using 'default' to handle reserved keywords like 'default' class name
   exportType: "default",
-  // Ignore patterns
   ignore: ["**/node_modules/**", "**/.next/**"],
-  // Log level
   logLevel: "error",
-  // Additional options
   additionalData: "",
-  // Implementation: 'sass' or 'sass-embedded'
-  // DOC: typed-scss-modules uses legacy Sass JS API internally, which causes deprecation warnings
-  // These warnings are harmless and do not affect functionality
-  // They will be resolved when typed-scss-modules updates to use the modern Sass API
-  // See: https://sass-lang.com/d/legacy-js-api
+  // DOC Deprecation warnings from typed-scss-modules using legacy Sass API are expected and harmless - if not resolved with dep updates, consider alternative approach
   implementation: "sass",
-  // Quote class names to handle reserved keywords
   quoteType: "single",
 };
