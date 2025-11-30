1a4cb619a6acbce876af366420de52b21ad2fe06
Diego Grassino <diegograssino@gmail.com>
2025-11-30 00:25:57 -0300

    Fix type import error

--- types/pages/pages.tsx
+++ types/pages/pages.tsx
@@ -1,5 +1,5 @@
 import { PageConfig } from "../shared/shared";
-import { FetchUsersResult } from "../users";
+import { FetchUsersResult } from "../users/users";
 
 export interface PageParamsProps {
   searchParams: Promise<Record<string, string | string[] | undefined>>;
